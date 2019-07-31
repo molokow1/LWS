9962d0a9df42969f05d7483d6790bd0ffb972182
Sean Wu <seanwu1995@gmail.com>
2019-07-31 19:56:45 +0800

    updated more form fields and html tempaltes

--- lws_web/lws_web_host/forms.py
+++ lws_web/lws_web_host/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 
 REGION_CHOICES = [
-    ('AU' , 'AU'),
-    ('EU' , 'EU'),
-    ('AS' , 'AS'),
+    ('AU', 'AU'),
+    ('EU', 'EU'),
+    ('AS', 'AS'),
 ]
 
 FREQ_CHOICES = [
@@ -26,13 +26,29 @@ SF_CHOICES = [
     (12, 12),
 ]
 
+
 class SimulationSettingsForm(forms.Form):
-    
-    region = forms.ChoiceField(label="Region", choices=REGION_CHOICES)
-    freq = forms.ChoiceField(label="Center Frequency", choices=FREQ_CHOICES)
-    bw = forms.ChoiceField(label="Bandwidth", choices=BW_CHOICES)
-    sf = forms.ChoiceField(label="Spreading Factor", choices=SF_CHOICES)
+    regional_settings_header = "Regional Transmission Settings"
+    select_region = forms.ChoiceField(label="Region", choices=REGION_CHOICES)
+    select_freq = forms.ChoiceField(
+        label="Center Frequency", choices=FREQ_CHOICES)
+    select_bw = forms.ChoiceField(label="Bandwidth", choices=BW_CHOICES)
+    select_sf = forms.ChoiceField(label="Spreading Factor", choices=SF_CHOICES)
 
-class EnvironmentSettingsForm(forms.Form):
+    environment_settings_header = "Environment Settings"
+    env_burial_depth = forms.DecimalField(
+        label="Burial Depth (m)", required=True, min_value=0, localize=True)
+    env_vwc = forms.DecimalField(
+        label="VWC (%)", required=True, min_value=0, localize=True)
+    env_sand = forms.DecimalField(
+        label="Sand (%)", required=True, min_value=0, localize=True)
+    env_clay = forms.DecimalField(
+        label="Clay (%)", required=True, min_value=0, localize=True)
 
-    pass
+    simulation_settings_header = "Simulation Parameters"
+
+    sim_duration = forms.DecimalField(
+        label="Simulation Duration (Hr)", required=True, min_value=0, localize=True)
+
+    sim_avg_send_time = forms.DecimalField(
+        label="Average Send Time (Mins)", required=True, min_value=0, localize=True)
